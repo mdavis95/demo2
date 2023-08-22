761235576378fcbed69211e2488477b05cfbe902
Matthew Davis <matt.davis@ascend-tech.us>
2023-08-22 08:16:30 -0400

    add a test with a map

--- src/main/java/com/example/Demo2Controller.java
+++ src/main/java/com/example/Demo2Controller.java
@@ -1,12 +1,18 @@
 package com.example;
 
-import io.micronaut.http.annotation.*;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
 
 @Controller("/demo2")
 public class Demo2Controller {
 
-    @Get(uri="/", produces="text/plain")
-    public String index() {
-        return "Example Response";
-    }
+	@Get(uri = "/", produces = "text/plain")
+	public String index() {
+		return "Example Response";
+	}
+
+	@Get(uri = "/tags", produces = "text/plain")
+	public Tags tags() {
+		return new Tags();
+	}
 }
---
+++ src/main/java/com/example/Tags.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.Map;
+
+public class Tags {
+	private Map<String, String> tags;
+
+	public Tags() {
+
+	}
+
+	public Map<String, String> getTags() {
+		return tags;
+	}
+
+	public void setTags(Map<String, String> tags) {
+		this.tags = tags;
+	}
+
+}
